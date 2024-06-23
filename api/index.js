e87426f1627a37be2a1645677d58fce4493d6c06
Chiranjeev Vishnoi <somu209e@gmail.com>
2024-06-23 23:00:47 +0530

    path changes

--- api/index.js
+++ api/index.js
@@ -4,8 +4,8 @@ import express from "express";
 import cors from "cors";
 import { json } from "express";
 import dotenv from "dotenv";
-import "./db/conn.js"; // Import the database connection setup
-import router from "./routes/router.js";
+import "../db/conn.js";
+import router from "../routes/router.js";
 import path from "path";
 dotenv.config(); // Load environment variables from .env file
 
@@ -14,7 +14,6 @@ const port = process.env.PORT || 6002;
 
 app.use(cors());
 app.use(json());
-
 
 app.get("/", (req, res) => {
   res.status(200).json("Server starts");
@@ -25,7 +24,7 @@ app.use("/api", router); // Use the router for handling requests
 app.get("/", (req, res) => {
   app.use(express.static(path.resolve(__dirname, "client", "build")));
   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
+});
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
